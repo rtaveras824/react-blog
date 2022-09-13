0ef994e548009e4ad61f6865f58fff4684866f70
Reuben Taveras <rtaveras824@hotmail.com>
2022-09-12 23:17:22 -0400

    Added more pages and react router

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,6 +1,8 @@
+import { Link } from 'react-router-dom';
 import './topbar.css';
 
 export default function TopBar() {
+	const user = false;
 	return (
 		<div className="top">
 			<div className="topLeft">
@@ -11,15 +13,41 @@ export default function TopBar() {
 			</div>
 			<div className="topCenter">
 				<ul className="topList">
-					<li className="topListItem">HOME</li>
-					<li className="topListItem">ABOUT</li>
-					<li className="topListItem">CONTACT</li>
-					<li className="topListItem">WRITE</li>
-					<li className="topListItem">LOGOUT</li>
+					<li className="topListItem">
+						<Link className="link" to="/">HOME</Link>
+					</li>
+					<li className="topListItem">
+						<Link className="link" to="/about">ABOUT</Link>
+					</li>
+					<li className="topListItem">
+						<Link className="link" to="/">CONTACT</Link>
+					</li>
+					<li className="topListItem">
+						<Link className="link" to="/write">WRITE</Link>
+					</li>
+					<li className="topListItem">
+						{ user && <Link className="link" to="/">LOGOUT</Link> }
+					</li>
 				</ul>
 			</div>
 			<div className="topRight">
-				<img className="topImg" src="https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U" alt="Profile picture" />
+				{ user ? (
+					<img className="topImg" src="https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U" alt="Profile picture" />
+					) : (
+					<ul className="topList">
+						<li className="topListItem">
+							<Link className="link" to="/login">
+								LOGIN
+							</Link>
+						</li>
+						<li className="topListItem">
+							<Link className="link" to="/register">
+								REGISTER
+							</Link>
+						</li>
+					</ul>
+					)}
+				
 				<i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
 			</div>
 		</div>
