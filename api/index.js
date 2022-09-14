1b3b3ba185e7362892f43c70f5ab41c34d3f7747
Reuben Taveras <rtaveras824@hotmail.com>
2022-09-14 15:58:24 -0400

    Uploaded api routs

--- api/index.js
+++ api/index.js
@@ -2,12 +2,37 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const authRoute = require('./routes/auth');
+const usersRoute = require('./routes/users');
+const postsRoute = require('./routes/posts');
+const categoriesRoute = require('./routes/categories');
+const multer = require('multer');
 
 dotenv.config();
+app.use(express.json()); 
 
 mongoose.connect(process.env.MONGO_URL)
 	.then(console.log('Connected to mongo'))
 	.catch((err) => console.error(err));
+
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, 'images');
+	}, filename: (req, file, cb) => {
+		cb(null, req.body.name);
+	}
+})
+
+const upload = multer({ storage: storage });
+app.post('/api/upload', upload.single('file'), (req, res) => {
+	res.status(200).json('File has been uploaded');
+});
+
+
+app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/posts', postsRoute);
+app.use('/api/categories', categoriesRoute);
 
 app.listen('3001', () => {
 	console.log('Backend is running');
